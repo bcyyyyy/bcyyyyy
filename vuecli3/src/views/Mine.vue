6fe6552311f8b7e32a1f2f98e0e5db125f68b289
bcyyyyy <1063967832@qq.com>
2018-08-14 16:52:22 +0800

    vuecli3
    Signed-off-by: bcyyyyy <1063967832@qq.com>

---
+++ vuecli3/src/views/Mine.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+    <h1>我是美少女</h1>
+
+        <router-link :to="{name:'test1',params:{name:'zhangsan',age:18}}">子路由1</router-link> |
+        <router-link to="/mine/test2/lisi/12">子路由2</router-link>
+        <router-view/>
+
+    </div>
+</template>
